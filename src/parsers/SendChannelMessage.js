1b82bdf350e35fea966ef5287d8c913e4e328630
WayvshockGD <pizza5803@gmail.com>
2021-09-28 02:40:18 +0000

    Fixed one parser (i guess)

--- src/parsers/SendChannelMessage.js
+++ src/parsers/SendChannelMessage.js
@@ -1,22 +1,17 @@
 
 /**
  * @param {string} content
+ * @param {Array<object>} json
  */
-module.exports = function(content) {
+module.exports = function(content, json) {
     let regex = /Send_channel_message then:/g;
-    let end_regex = /Send_channel_message then: [A-z] :end/;
-
-    let sliced = content.slice(content.indexOf("Send_channel_message"));
-    let sliced_then = sliced.slice(0, sliced.lastIndexOf("then:") + 5);
-    let sliced_end = content.slice(content.indexOf(":end"));
+    let slice_content = "Send_channel_message then:";
+    let sliced = content.slice(content.indexOf(slice_content), content.lastIndexOf(slice_content) - 12);
+    console.log(sliced)
+    let keys = content.slice(content.indexOf(slice_content) + 27, content.lastIndexOf(slice_content) - 5);
+    sliced = sliced.slice(0, content.indexOf("then:") + 5);
     
-    if (!regex.test(sliced_then)) {
-        throw new SyntaxError(`"Send_channel_message then" is not correct. recieved: ${sliced_then}`);
-    }
-
-    if (!sliced_then.length) {
-        throw new SyntaxError(":end is not at the end of 'Send_channel_message'.");
-    }
-    
-    console.log(sliced_end)
+    if (!regex.test(sliced)) {
+        throw new SyntaxError(`"Send_channel_message then" is not correct. recieved: ${sliced}`);
+    };
 }
